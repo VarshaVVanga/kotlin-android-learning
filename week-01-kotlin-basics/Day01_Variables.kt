672aaf4cb1b9513eb2825922d7dfe59f29b71f73
VarshaVVanga <47239796+VarshaVVanga@users.noreply.github.com>
2026-09-17 18:32:29 +1000

    Add Kotlin variables practice

---
+++ week-01-kotlin-basics/Day01_Variables.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val developerName = "Varsha"
+    var studyMinutes = 30
+    val learningTopic = "Kotlin variables and basic types"
+
+    println("Developer: $developerName")
+    println("Today's topic: $learningTopic")
+    println("Study target: $studyMinutes minutes")
+
+    studyMinutes = 20
+    println("Actual study time: $studyMinutes minutes")
+}
